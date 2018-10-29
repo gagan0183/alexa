0c61132efa257fb169aa6c1f0c93a4184414c1e5
Gagan <g.deepsingh1@gmail.com>
2018-10-29 17:43:25 +0530

    first event

--- Greetings/lambdaFun/index.js
+++ Greetings/lambdaFun/index.js
@@ -3,32 +3,65 @@
 */
 
 exports.handler = function(event, context) {
-    var request = event.request;
+    try {
+        var request = event.request;
 
-    /*
-        1) 3 types of requests
-            i)   LaunchRequest       Ex: "Open greeter"
-            ii)  IntentRequest       Ex: "Say hello to John" or "ask greeter to say hello to John"
-            iii) SessionEndedRequest Ex: "exit" or error or timeout
-    */
+        /*
+            1) 3 types of requests
+                i)   LaunchRequest       Ex: "Open greeter"
+                ii)  IntentRequest       Ex: "Say hello to John" or "ask greeter to say hello to John"
+                iii) SessionEndedRequest Ex: "exit" or error or timeout
+        */
 
-    if(request.type === "LaunchRequest") {
-        context.succeed(buildResponse({
-            speechText: "Welcome to Greeting skill. Using our skill you can greet your guests. whom you want to greet?  ",
-            repromptText: "You can say for example, say hello to John",
-            endSession: false
-        }));
+        if(request.type === "LaunchRequest") {
+            context.succeed(buildResponse({
+                speechText: "Welcome to Greeting skill. Using our skill you can greet your guests. whom you want to greet?  ",
+                repromptText: "You can say for example, say hello to John",
+                endSession: false
+            }));
+        }
+        else if(request.type === "IntentRequest") {
+            let options = {};
+            if(request.intent.name === "HelloIntent") {
+                let name = request.intent.slots.FirstName.value;
+                options.speechText = "Hello " + name + ". ";
+                options.speechText += getWish(); 
+                options.endSession =  true;
+                context.succeed(buildResponse(options));
+            }
+            else {
+                throw "unknown intent";
+            }
+        }
+        else if(request.type === "SessionEndedRequest") {
+
+        }
+        else {
+            throw "unknown intent type";
+        }
     }
-    else if(request.type === "IntentRequest") {
-
-    }
-    else if(request.type === "SessionEndedRequest") {
-
-    }
-    else {
-        context.fail("unknown intent type");
+    catch(e) {
+        context.fail("exception ", e);
     }
 };
+
+function getWish() {
+    var myDate = new Date();
+    var hours = myDate.getUTCHours() + 5.30;
+    if(hours < 0) {
+        hours = hours + 24;
+    }
+
+    if(hours < 12) {
+        return "Good Morning";
+    }
+    else if(hours < 18) {
+        return "Good Evening";
+    }
+    else {
+        return "Good Night";
+    }
+}
 
 function buildResponse(options) {
     var response = {
